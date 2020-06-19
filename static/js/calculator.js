5e7a98497b89b48ede84f5cb2a5e273b1bea568d
Bernadzikovskiy Slavik <vbernadzikovskiy@gmail.com>
2020-06-19 17:07:58 +0300

    changed functionality after pressing equal and added validations

--- static/js/calculator.js
+++ static/js/calculator.js
@@ -1,10 +1,32 @@
-allowedKeys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "*", "/", ".", "=", "c"]
+const allowedKeys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "*", "/", ".", "=", "c"];
+const validation = /(?:(?:^|[-+_*/])(?:\s*-?\d+(\.\d+)?(?:[eE][+-]?\d+)?\s*))+$/;
+var isEqualed = false;
 
 document.onkeypress = function (e) {
     keyPressed = String.fromCharCode(e.which);
     console.log(keyPressed);
     if (allowedKeys.includes(keyPressed)) {
         console.log("HERE");
-        document.getElementById(keyPressed).click();
+        document.getElementById(keyPressed).click(function (event) {
+            event.stopPropagation();
+        });
     }
 };
+
+function calculate(expr) {
+    isEqualed = true;
+    try {
+        return (exprIsValid(expr) ? eval(expr) : "Expression is invalid!");
+    } catch (error) {
+        return "Expression is invalid!";
+    }
+}
+
+function exprIsValid(expr) {
+    return validation.test(expr);
+}
+
+function clickButton(key) {
+    isEqualed ? document.calc.txt.value = key : document.calc.txt.value += key;
+    isEqualed = false;
+}
