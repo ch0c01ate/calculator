e94999266eaca1cf9dbfbbd1b373444661301970
Bernadzikovskiy Slavik <vbernadzikovskiy@gmail.com>
2020-06-24 14:49:19 +0300

    bug fixes

--- calculator.py
+++ calculator.py
@@ -3,8 +3,11 @@ import re
 
 
 def calculate(expr):
-    return int(ne.evaluate(expr)) if validate_expression(
-        expr) else "Invalid expression!"
+    try:
+        return ne.evaluate(expr).item() if validate_expression(
+            expr) else "Invalid expression!"
+    except:
+        return "Invalid expression!"
 
 
 def validate_expression(expr):
